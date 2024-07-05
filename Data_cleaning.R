95a16637b4f4d86cf8cf14b649bff301f6d6ce2c
deathdolphin <armandusmaximus@gmail.com>
2024-07-05 14:24:19 -0400

    Creación df2_cleaned

--- Data_cleaning.R
+++ Data_cleaning.R
@@ -4,11 +4,13 @@ pacman::p_load(tidyverse, pacman, DescTools)
 
 DF1 <- na.omit(Smoker_G7) %>% 
   filter(current_smoker == "yes")
+  DF1$sex <- factor(DF1$sex)
+  DF1$current_smoker <- factor(DF1$current_smoker)
 
 summary(DF1)
 
-20 + (1.5 * IQR(DF1$cigs_per_day)) #Límite superior
-10 - (1.5 * IQR(DF1$cigs_per_day)) #Límite Inferior
+(20 + (1.5 * IQR(DF1$cigs_per_day))) #Límite superior
+(10 - (1.5 * IQR(DF1$cigs_per_day))) #Límite Inferior
 
 # Winsorize cigs_per_day
 
@@ -45,8 +47,18 @@ df1_cleaned <- DF1 %>%
 df1_cleaned$systolic_pressure <- as.numeric(df1_cleaned$systolic_pressure)
 df1_cleaned$dystolic_pressure <- as.numeric(df1_cleaned$dystolic_pressure)
 
-
-
+#Creación df2_cleaned
+y <- Smoker_G7$cigs_per_day
+cigs_cleaned2 <-  Winsorize(y, minval = NULL, maxval = NULL,
+                            probs = c(0,0.96), na.rm = TRUE, 
+                            type = 1)
+df2_cleaned <- Smoker_G7 %>%
+  mutate(cigs_per_day = cigs_cleaned2) %>%
+  separate_wider_delim(
+    blood_pressure, delim = "/",
+                       names= c("systolic_pressure", "dystolic_pressure"))
+df2_cleaned$systolic_pressure <- as.numeric(df2_cleaned$systolic_pressure)
+df2_cleaned$dystolic_pressure <- as.numeric(df2_cleaned$dystolic_pressure)
 
   
   
